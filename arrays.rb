34eb62116957aefd93c66185aa6bd84a5243df0c
Nick Clark <nick.clark@coderacademy.edu.au>
2018-08-28 01:57:06 +1000

    Added solutions.

--- arrays.rb
+++ arrays.rb
@@ -4,11 +4,38 @@
 kardashians = ['Kim', 'Robert', 'Khloé', 'Rob', 'Kourtney']
 
 # 1) Find two ways to access and print the 'Kim' value.
+puts kardashians[0]
+puts kardashians[-5]
+kardashians.at(0)
 
 # 2) Find two ways to remove the male names from the Kardashians.
 
+# Method 1:
+kardashians.delete_at(1)
+kardashians.delete_at(2)
+puts kardashians
+#Method 2:
+kardashians.delete('Rob')
+kardashians.delete('Robert')
+puts kardashians
+
 # 3) Remove one Kardashian, print the names and then add them back to the Array:
+removed_kardashian = kardashians.shift
+puts kardashians
+kardashians.push(removed_kardashian)
+puts kardashians
+
 
 # 4) Take the last Kardashian and make them the first, print the result.
+last_kardasian = kardashians.pop
+kardashians.insert(0, last_kardasian)
+puts kardashians
 
-# 5) Find a way to count the number of items in the array that begin with the letter 'K'
+# 5) Find a way to count the Kardashians in the array that begin with the letter 'K'
+count = 0
+kardashians.each do |kardashian|
+  if kardashians.first
+    count += 1
+  end
+end
+puts "There are #{count} Kardashians with names beginning with K."
--- loops.rb
+++ loops.rb
@@ -1,18 +1,57 @@
-# For questions 1 and 2 below use the following array:
+# F|| questions 1 and 2 below use the following array:
 shopping_list = ['cheese', 'toilet paper', 'taco shells', 'sushi rolls', '3 avocados', 'soap', 'sparkling mineral water', '2 pineapples', 'toothpaste']
 
 # 1) Use .each to loop through each item in the shopping_list and capitalise
-#    all words in each item. Hint: work smarter not harder - check Stack Overflow
+#    all w||ds in each item. Hint: w||k smarter not harder - check Stack Overflow
+
+shopping_list.each do |item|
+  item = item.split.map(&:capitalize).join(' ')
+  puts item
+end
+
 
 # 2) Use while to loop through the shopping list and divide the shopping list
-#    into multiple arrays according to item type. Print all of the arrays with a divider
-#    between each and a total item count for each item.
+#    into multiple arrays acc||ding to item type. Print all of the arrays with a divider
+#    between each and a total item count f|| each item.
 
 bathroom_items = []
 fruit = []
 dairy = []
 meals = []
 beverages = []
+i = 0
+while i < shopping_list.length
+  if shopping_list[i] == 'toilet paper' || shopping_list[i] == 'soap' || shopping_list[i] == 'toothpaste'
+    bathroom_items.push(shopping_list[i])
+  elsif shopping_list[i] == '3 avocados' || shopping_list[i] == '2 pineapples'
+    fruit.push(shopping_list[i])
+  elsif shopping_list[i] == 'cheese'
+    dairy.push(shopping_list[i])
+  elsif shopping_list[i] == 'taco shells' || shopping_list[i] == 'sushi rolls'
+    meals.push(shopping_list[i])
+  elsif shopping_list[i] == 'sparkling mineral water'
+    beverages.push(shopping_list[i])
+  end
+  i += 1
+end
+divider = '============================'
+puts 'Shopping List'.center(10, '=')
+puts divider
+puts '==== Bathroom Items'
+puts bathroom_items
+puts divider
+puts '==== Fruit'
+puts fruit
+puts divider
+puts '==== Dairy'
+puts dairy
+puts divider
+puts '==== Meals'
+puts meals
+puts divider
+puts '==== Beverages'
+puts beverages
+puts divider
 
 # 3) Create an array of at least 20 integers.
 #       Use a for loop to:
@@ -21,8 +60,18 @@ beverages = []
 #         - create an array of the same values as floats
 #         - create an array of values that are multiplied by a randomly generated number
 
+integers = [21, 22, 15, 2, 3, 2, 6, 78, 33, 2, 8, 9, 34, 88, 99, 10, 45, 67, 87, 50, 11, 14, 62, 7]
+total = 0
+count = integers.count
+for int in integers
+  total += int
+end
+average = total / count
+puts 'Average of integers is: ' + average.to_s
+
 # EXTENSION TASK
 # Use a for loop to go through each item in the shopping_list, remove any number in the
 # string and replace it with its corresponding word. For example: '3 avocados' will
 # become 'three avocados'.
 # Consider using RegEx.
+
